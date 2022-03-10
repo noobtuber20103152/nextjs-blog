a962d301f20910c37821fe3bb7e563b85be3737c
Ujjawal <ujjawalrachhoya100@gmail.com>
2022-03-10 22:43:00 +0530

    updated

--- pages/BlogContent/BlogContentData/React.js
+++ pages/BlogContent/BlogContentData/React.js
@@ -11,7 +11,7 @@ const data = [
     },
        {
            "Introduction about JSX":[
-               {"What is JSX?":"const element = <h1>Hello World!</h1> \n//About syntex is neither HTML or neither JavaScript it is call jsx(JavaScript XML)"},
+               {"What is JSX?":"const element = <h1>Hello World!</h1> \n//Above syntex is neither HTML or neither JavaScript it is call jsx(JavaScript XML)"},
            ]
        },
     {
@@ -28,7 +28,7 @@ const data = [
     },
     {
         "Props and states in react":[
-            {"Props - It is an object which stores the value of attributes of a tag and work similar to the HTML attributes.":"function Hello(props){\n   return <h1>Hello, {props.name}</h1>\n}\nconst element = <Welcome name='Shruti' />;\nReactDOM.render(\n  element,document.getElementById('root')\n  )\n}"},
+            {"Props - It is an object which stores the value of attributes of a tag and work similar to the HTML attributes.":"function Hello(props){\n   return <h1>Hello, {props.name}</h1>\n}\nconst element = <Welcome name='Shruti' />;\nReactDOM.render(\n  element,\n  document.getElementById( 'root' )\n  )\n}"},
 
             {"State - The state object is where you store property values that belongs to the component":"function time(){\n   const element = (\n     <div>\n      <h1>Hello, World</h1>\n      <h2>It is {new Date().toLocaleTimeString()} </h2>\n    </div>\n);\nReactDOM.render(\n   element,\n   document.getElementById('root'  )\n  );\n}\nsetInterval(tick, 1000);"}
         ]
@@ -40,7 +40,20 @@ const data = [
     },
     {
         "Conditional rendering":[
-           { "In React you can render components and elements based on condition.":"let login = ()=>{\n  return <h1>You logged in successfully</h1>\n}\nlet signup = ()=>{\n  return <h1>Please sign up</h1>\n}\nexport default APP(){\n   let condition = true;\n   if(condition){\n   return login\n  }\n   else{\n    return signup\n  }\n"}
+           { "In React you can render components and elements based on condition.":"let login = ()=>{\n  return <h1>You logged in successfully</h1>\n}\nlet signup = ()=>{\n  return <h1>Please sign up</h1>\n}\nexport default APP(){\n   let condition = true;\n   if(condition){\n    return login\n  }\n   else{\n    return signup\n  }\n"}
+        ]
+    },
+    {
+        "Iterate items in list":[
+            {"Iterate items in any lise we are use map function in react":"const data = [1,2,3,4,5]\ndata.map((element)=>{\n   console.log(element)\n})"},
+            {"Rendering components":"const data = [1,2,3,4,5,6,7]\nconst items = data.map((element)=>{\n  <h1>Heading {element}</h1>\n})\nReactDOM.render(\n    <ul>{items}</ul>\n    document.getElementById('root')\n);"}
+        ]
+    },
+
+    {
+        "Hooks in React":[
+            {"useState hook - useState hook use to update the state of variable dynamically":"import React, {useState} from 'react'\n\nfunction App(){\n  // Declare a new state variable, which we'll call 'count'\n   const [count, setcount] = useState(0)\n   return (\n      <div>\n       <p>You clicked {count} times</p>\n      <button onClick={()=>setcount(count+1)}> Click me </button>\n     </div>\n   );\n}\n//On every click on button count increment by 1 and update. "},
+            {"useEffect hook - useEffect hook lets you perform side effects in function based components":"import React, {useState, useEffect} form 'react'\nfunction App(){\n  cont [count, setcount] = useState(0);\n  useEffect(()=>{\n   document.title=`you clicked ${count} times1;\n },[]);\n\n  return (\n    <div>     \n     <p> You clicked {count} times </p>\n     <button onClick={()=>setcount(count+1)}></button>\n   </div>\n  );\n}"}
         ]
     }
 ];
--- pages/Blogs/BlogJSON.js
+++ pages/Blogs/BlogJSON.js
@@ -7,7 +7,7 @@ const data = [
             "date": "12 Dec 2021",
             "desc": "In this post, we will see a comprehensive list of all the MongoDB commands you will ever need as a MongoDB beginner. This list covers almost all the most used commands in MongoDB.",
             "link": "MongoDB-Cheat-Sheet",
-            "imgLink":"https://cdn.educba.com/academy/wp-content/uploads/2018/12/Is-MongoDB-NoSQL.jpg"
+            "imgLink": "https://cdn.educba.com/academy/wp-content/uploads/2018/12/Is-MongoDB-NoSQL.jpg"
         },
     },
     {
@@ -18,7 +18,7 @@ const data = [
             "date": "13 Dec 2021",
             "desc": "In this cheat sheet we will provide common JavaScript concepts and syntex for quick revision for web developers. If you like this cheat sheet please give us your suggetion for improve more this cheat sheet.",
             "link": "JavaScript-Cheat-Sheet",
-            "imgLink":"https://cyberhoot.com/wp-content/uploads/2020/07/Free-Courses-to-learn-JavaScript.jpg"
+            "imgLink": "https://cyberhoot.com/wp-content/uploads/2020/07/Free-Courses-to-learn-JavaScript.jpg"
         }
     },
     {
@@ -29,10 +29,10 @@ const data = [
             "date": "14 Dec 2021",
             "desc": "React is a JavaScript based library which is used to make awesome fronted single page web application. In this blogpost we will share React JS cheat sheet with you. I hope you love it.",
             "link": "React-Cheat-Sheet",
-            "imgLink":"https://www.valuecoders.com/blog/wp-content/uploads/2016/08/react.png"
+            "imgLink": "https://www.valuecoders.com/blog/wp-content/uploads/2016/08/react.png"
         }
     },
-    
+
 ]
 
 export default data;
